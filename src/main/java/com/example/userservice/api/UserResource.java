4f37ef23ba21a73a65ea3a753ecb35267a0e77ff
vladyslavkosenko <vaduha91@gmail.com>
2022-10-04 16:55:19 +0300

    first commit

---
+++ src/main/java/com/example/userservice/api/UserResource.java
@@ -0,0 +1,23 @@
+package com.example.userservice.api;
+
+import com.example.userservice.domain.User;
+import com.example.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class UserResource {
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
+}
